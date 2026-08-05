e1727dde8646a614333a1d4f66549ea0e8e2187c
safaahvivo68-a11y <safaahvivo68@gmail.com>
2026-08-05 08:24:42 +0700

    Add files via upload

---
+++ program_pertama.py
@@ -0,0 +1,21 @@
+# Program Pertamaku
+# Nama: haqi
+# kelas: X RPL 2
+
+print("-------------------------")
+print("SELAMAT DATANG DI PYTHON!")
+print("-------------------------")
+print()
+print("Program ini dibuat oleh:", "Haqi")
+print("Sekolah: SMK TJP TUBAN")
+print("Tahun  :",2026)
+print(10 + 5) #penjumlahan
+print(20 - 8) #pengurangan 
+print(4 * 7)  #perkalian
+print(15 / 4) #pembagian
+print(15 // 4)#pembagian bulat
+print(15 % 4) #sisa bagi
+print(2 ** 8) #perpangkatan
+a = 24
+b = 7
+print("penjumlahan:", a + b)
