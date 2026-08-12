75a9658a71293df76d6ae8cbe0db7e7c3200ada3
safaahvivo68-a11y <safaahvivo68@gmail.com>
2026-08-12 08:07:54 +0700

    Add files via upload

---
+++ bab3.py
@@ -0,0 +1,19 @@
+Nama = "Al"
+Umur = "15"
+Tinggi = "175"
+Lulus = "True"
+
+print("Nama : ", Nama)
+print("umur : ", Umur)
+print("Tinggi : ", Tinggi)
+print("Lulus : ", Lulus)
+
+Produk = "Sepatu Lari"
+Stock = "15"
+Harga = "250"
+Tersedia = "True"
+
+print("Nama Produk : ", Produk)
+print("Stok Tersedia : ", Stock)
+print("Harga : ", Harga)
+print("Tersedia di Toko = ", Tersedia)
