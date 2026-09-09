aa081735806c7d217d632f748633b4e10e48265b
safaahvivo68-a11y <safaahvivo68@gmail.com>
2026-09-09 09:28:49 +0700

    Add files via upload

---
+++ fizzbuzz.py
@@ -0,0 +1,13 @@
+n = int(input("masukan angka: "))
+i = 1
+while i <= n :
+    if i % 3 == 0 and i % 5 == 0 :
+        print("FizzBuzz")
+    elif i % 3 == 0 :
+        print("Fizz")
+    elif i % 5 == 0 :
+        print("Buzz")
+    else :
+        print(i)
+    i+=1
+        
